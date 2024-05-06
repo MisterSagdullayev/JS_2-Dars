041a854d3f66d5d1a826c580cd05f8f7fecc2ac3
Jasur Sagdullayev <168184569+MisterSagdullayev@users.noreply.github.com>
2024-05-06 17:01:22 +0500

    JS 2-Dars
    Zeromax IT-Center

---
+++ js/main.js
@@ -0,0 +1,52 @@
+/* if      => agar          */
+/* else    => u holda       */
+/* else if => yoki bo'lmasa */
+
+/*
+if(45 <= 44){
+    console.log("Solishtirilayotgan son 45ga teng yoki katta!");
+}else{
+    console.log("Solishtirilayotgan son 45ga teng emas yoki katta emas!");
+}
+*/
+
+/*
+if (45 == 46){
+    console.log("1-shart bajarildi!");
+}else if (45 == 44){
+    console.log("2-shart bajarildi!");
+}else{
+    console.log("Yuqoridagi shartlardan birortasi bajarilmadi!");
+}
+*/
+
+/*
+let a = 45;
+if (a >= 46){
+    console.log("a berilgan sondan kichik yoki teng!");
+}else{
+    console.log("a ning qiymati topilmadi!");
+}
+*/
+
+let a = 46;
+let b = 10;
+let c = 19;
+let d = 49;
+
+/*
+if (a == 46 && b == 10 && c == 19 && d == 49){
+    console.log("Shartlarning hammasi to'g'ri!");
+}else{
+    console.log("Shartlarning hammasi yoki kamida bittasi noto'g'ri!");
+}
+*/
+
+/*
+if (a == 46 || b == 10 || c == 19 || d == 49){
+    console.log("Shartlarning kamida bittasi yoki hammasi to'g'ri!");
+}
+else{
+    console.log("Shartlarning hammasi noto'g'ri!");
+}
+*/
